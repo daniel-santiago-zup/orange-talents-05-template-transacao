11ca3774883377e218be7c8d448922bf84755a96
Daniel Ribeiro Santiago <daniel.santiago@zup.com.br>
2021-06-18 10:32:14 -0300

    primeiro commit com configurações iniciais do projeto

---
+++ servico-transacao/src/test/java/br/com/zup/servicotransacao/ServicoTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.servicotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
